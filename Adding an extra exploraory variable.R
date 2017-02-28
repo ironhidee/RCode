d471b0ab138f16c851262d2d40936fb6001a4730
ironhidee <ironhide.ec@gmail.com>
2017-02-28 12:42:26 +0530

    Change

---
+++ Adding an extra exploraory variable.R
@@ -0,0 +1,18 @@
+require(mosaicData)
+
+# Add bogus column to CPS85 (don't change)
+CPS85$bogus <- rnorm(nrow(CPS85)) > 0
+
+# Make the base model
+base_model <- lm(wage ~ educ + sector + sex, data = CPS85)
+
+# Make the bogus augmented model
+aug_model <- lm(wage ~ educ+ sector + sex + bogus, data = CPS85)
+
+# Find the MSE of the base model
+mean((CPS85$wage - predict(base_model, newdata = CPS85)) ^ 2)
+
+# Find the MSE of the augmented model
+mean((CPS85$wage - predict(aug_model, newdata = CPS85)) ^ 2)
+
+mean(with(CPS85, wage - predict(aug_model, newdata = CPS85)) ^ 2) #Another method for cycling through data
